7786c9dc6897734d7a5579799373f6221166a812
ozmeri <ozmeri@users.noreply.github.com>
2026-04-12 19:16:38 +0300

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-﻿// Firebase Console > Project settings > Your apps > Web app config bilgilerini buraya yapistir.
+﻿// Firebase Console > Project settings > Your apps > Web app config.
 // Bu ayarlar gizli sifre degildir; guvenlik Firestore Rules ve Authentication ile saglanir.
 window.fitFirebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBn_R8mmo4hg5LJk8SnH2CAMfoed1LMMSc",
+  authDomain: "yemek-tarifi-uygulamasi-5943f.firebaseapp.com",
+  projectId: "yemek-tarifi-uygulamasi-5943f",
+  storageBucket: "yemek-tarifi-uygulamasi-5943f.firebasestorage.app",
+  messagingSenderId: "464313857330",
+  appId: "1:464313857330:web:1819d891a1a972ba65932b",
+  measurementId: "G-YYLSZYQ6NL"
 };
